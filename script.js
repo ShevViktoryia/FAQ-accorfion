051d413917961c45814f0f73aea3c1908f56f53f
Victoria <shevvictoryia@gmail.com>
2024-08-31 23:35:34 +0300

    feat: finish project

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ data.forEach((item) => {
   const title_text = document.createElement("h3");
   title_text.textContent = item.question;
   const svg = document.createElement("span");
+  svg.className = "arrow";
   svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
   <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659"/>
 </svg>`;
